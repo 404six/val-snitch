5000855b224821b2eb89b68a109a0010888f73c9
404six <lucasmoraisc16@gmail.com>
2024-07-30 15:27:56 -0300

    feat(constants): Add the default client plataform string

--- internal/constants/auth.go
+++ internal/constants/auth.go
@@ -9,6 +9,8 @@ type LogInfo struct {
 	Client_version string
 }
 
+const DefaultClientPlatformString = "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"
+
 var Ssid_regex = regexp.MustCompile(`name:\s*\"ssid\"\s*[\s\S]*?value:\s*\"([^\"]+)\"`)
 
 var (
